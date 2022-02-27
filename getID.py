b1675519eb0fc99e85719b07f0454f4a5a79fe52
spicypunch <72846127+spicypunch@users.noreply.github.com>
2022-02-27 13:28:37 +0900

    Add files via upload

---
+++ getID.py
@@ -0,0 +1,20 @@
+import base64
+from api import decodeValue
+
+
+def main():
+    tenantID = input('본인의 TenantID를 입력해주세요: ')
+    userID = input('본인의 클라우드 ID를 입력해주세요: ')
+    passwd = input('본인의 비밀번호를 입력해주세요: ')
+
+    tenantIDencode = tenantID.encode('ascii')
+    userIDencode = userID.encode('ascii')
+    passwdencode = passwd.encode('ascii')
+
+    tenantIDbase = base64.b64encode(tenantIDencode)
+    userIDbase = base64.b64encode(userIDencode)
+    passwdbase = base64.b64encode(passwdencode)
+    decodeValue(tenantIDbase, userIDbase, passwdbase)
+
+if __name__ == "__main__":
+    main()
